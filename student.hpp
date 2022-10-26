2abbdb69932130a2e32165e7a144a0d95adb232f
roque63 <roque@itesm.mx>
2022-10-26 13:46:15 -0500

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -1,15 +1,22 @@
 #pragma once // prevents multiple definitions
 
 #include <iostream> // print library
+#include <string>
 
-// Exercise 1: implementa la funciónque hace operaciones con números implements function 'add', that adds two numbers
-int operaNumeros(int a, int b, string operador );
+// Exercise 1: implementa la funciónque lleve a cabo
+// Función que recibe  dos números y la opcion de 3 opciones de un menú:
+// • S retornar la suma de los dos números
+// • R retornar la resta de los dos números (el primero menos el segundo)
+// • M retornar la multiplicación de los dos números
+// • En caso de no recibir una opción válida, la función retornará -1
+/
+int operaNumeros(int a, int b, std::string opcion );
 
 
 // ===============
 // implementations
 
-int operaNumeros(int a, int b, string operador )
+int operaNumeros(int a, int b, std::string opcion )
 {
-     return 0; // TODO: fix  
+     return -1; // TODO: fix  
 }
